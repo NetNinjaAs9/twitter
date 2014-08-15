62889629406b244152856069c7bce88d11a77c9f
Asylzhan Akynova <asylzhan07@gmail.com>
2014-08-15 15:02:33 +0000

    Chapter 7

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,17 @@
 Rails.application.routes.draw do
   
   match 'signup', to: "users#new", via: :get
-  
+  match 'signin', to: "sessions#new", via: :get
+  match 'signout', to: 'sessions#destroy', via: :delete
   root 'static_pages#home'
 
   match '/help', to: 'static_pages#help', via: :get
 
   match '/about', to: 'static_pages#about', via: :get
+  match '/contact', to: 'static_pages#contact', via: :get
 
+  resources :sessions, only: [:new, :create, :destroy]
   resources :microposts
-
   resources :users
 
   # The priority is based upon order of creation: first created -> highest priority.
