8834761fc431fbdd9f8c9893a6db7f0cc3683ea2
Asylzhan Akynova <asylzhan07@gmail.com>
2014-08-15 15:09:04 +0000

    Merge

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,15 +1,7 @@
 module UsersHelper
-<<<<<<< HEAD
-=======
-  
->>>>>>> filling-in-layout
   def gravatar_for(user)
     gravatar_id = Digest::MD5::hexdigest(user.email.downcase)
     gravatar_url = "https://secure.gravatar.com/avatar/#{gravatar_id}"
     image_tag(gravatar_url, alt: user.name, class: "gravatar")
   end
-<<<<<<< HEAD
 end
-=======
-end
->>>>>>> filling-in-layout
