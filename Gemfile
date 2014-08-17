8474bca4f51d6fc9bfb421599bae97f9d3ed5ce3
Asylzhan Akynova <asylzhan07@gmail.com>
2014-08-17 21:43:48 +0000

    Finish user edit, update, index, and destroy actions

--- Gemfile
+++ Gemfile
@@ -16,6 +16,9 @@ gem 'turbolinks', '1.1.1'
 gem 'jbuilder', '1.0.2'
 gem 'bootstrap-sass', '2.3.2.0'
 gem 'bcrypt-ruby', '3.1.2'
+gem 'faker', '1.1.2'
+gem 'will_paginate', '3.0.4'
+gem 'bootstrap-will_paginate', '0.0.9'
 
 group :doc do
   gem 'sdoc', '0.3.20', require: false
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140815144433) do
+ActiveRecord::Schema.define(version: 20140817212856) do
 
   create_table "microposts", force: true do |t|
     t.string   "content"
@@ -27,6 +27,7 @@ ActiveRecord::Schema.define(version: 20140815144433) do
     t.datetime "updated_at"
     t.string   "password_digest"
     t.string   "remember_token"
+    t.boolean  "admin",           default: false
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
