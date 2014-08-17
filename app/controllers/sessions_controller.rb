8f705969af34421b133a4445ead599ce7c27824d
Asylzhan Akynova <asylzhan07@gmail.com>
2014-08-17 20:24:53 +0000

    Finish sign in

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -8,8 +8,14 @@ class SessionsController < ApplicationController
       sign_in user
       redirect_to user
     else
-      flash[:error] = "Неверный логин и/или пароль"
-      render "new"
+      flash.now[:error] = 'Invalid email/password combination'
+      render 'new'
+    end
   end
+  
+   def destroy
+    sign_out
+    redirect_to root_url
   end
+  
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,13 +10,15 @@ class UsersController < ApplicationController
   
   def create
     @user = User.new(user_params)
-   if @user.save
-     flash[:success] = "Регистрация прошла успешно!"
+    if @user.save
+      sign_in @user
+      flash[:success] = "Welcome to the Sample App!"
       redirect_to @user
     else
       render 'new'
     end
   end
+  
   private
   
     def user_params
